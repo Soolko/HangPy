8fa5b661fac2eb5741d8d9a84b206866954bce4d
Myles Dunstan <jegjagg@gmail.com>
2019-11-13 03:58:38 +0000

    Launch scripts, Bash is recommended.

---
+++ Launch.bat
@@ -0,0 +1,9 @@
+@echo off
+rem This is completely untested.
+
+echo "I would recommend running this program through the .sh file in PowerShell."
+echo "This is because I have not tested this program on Windows."
+echo "You can terminate this now and try that if you want to."
+pause
+
+python -u Hangman.py
